36568074808372f70365d30bbd6257036cbdbb15
Soru Boru <113847544+Soruboru007@users.noreply.github.com>
2025-06-25 04:42:25 +0000

    requireの使ってみた＆配列の概念を理解

---
+++ javascript-basic/array.js
@@ -0,0 +1,12 @@
+let fruit = ["apple","banana","orange"];
+console.log(fruit[1]);
+
+let user = {name:"taro",age:19};
+console.log(user.age);
+
+let animal = ["dog","cat","bird"];
+
+for(let i = 0; i < animal.length; i++)
+{
+    console.log(animal[i]);
+}
